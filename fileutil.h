e96c0a4c520d34ea6cc9664831f0aed336d61e30
454270186 <sc21ey@leeds.ac.uk>
2023-12-02 20:34:56 +0800

    feat: add xml parser

--- fileutil.h
+++ fileutil.h
@@ -1,11 +1,24 @@
 #ifndef FILEUTIL_H
 #define FILEUTIL_H
 
+#include <string>
+
+#include "tinyxml2.h"
+
+using std::string;
+
 
 // FileUtil is the class for providing file operation
 class FileUtil {
 public:
-    FileUtil();
+    FileUtil(const string& XMLFilePath);
+
+    void PrintAll();
+    string GetVideosPathByListName(const string& listname);
+
+private:
+    string XMLFilePath_;
+    tinyxml2::XMLDocument xmlParser_;
 };
 
 #endif // FILEUTIL_H
