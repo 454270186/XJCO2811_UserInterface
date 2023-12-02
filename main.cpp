4f2ac7d0a1b5d8bb1daa8a4d0bcace3c6ef8aaee
Deck-Wang416 <sc212yw@leeds.ac.uk>
2023-12-02 21:14:43 +0800

    fix: solve main.cpp conflict

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <QApplication>
 
 #include "mainwindow.h"
-#include "listset.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
     MainWindow w;
     w.show();
-
-    ListSet s;
-    s.show();
 
     app.exec();
 }
