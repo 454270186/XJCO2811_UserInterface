dad1b68a8d9579043260e685c2db04953804c922
romanticthemeRT <sc21z2y@leeds.ac.uk>
2023-12-03 17:12:11 +0800

    merge: merge from main

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,5 +16,4 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
 
