22ceab2d35f05cd48ceb55179b49ff93415ae669
Erfei Yu <105307186+454270186@users.noreply.github.com>
2023-12-07 19:57:02 +0800

    feat: modify logic of add new button, add button selected style

--- listset.cpp
+++ listset.cpp
@@ -1,19 +1,17 @@
-#include <QPushButton> 
 #include <QMessageBox>
+#include <QPushButton>
+#include <iostream>
 
-#include "formhandler.h"
-#include "listset.h"
-#include "ui_listset.h"
+#include <QDebug>
 #include <QListWidgetItem>
 #include <QPushButton>
 #include <QVBoxLayout>
-#include <QDebug>
 #include <string>
+#include "formhandler.h"
+#include "listset.h"
+#include "ui_listset.h"
 
-ListSet::ListSet(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::ListSet)
-{
+ListSet::ListSet(QWidget* parent) : QMainWindow(parent), ui(new Ui::ListSet) {
     ui->setupUi(this);
     listLayout = ui->scrollAreaWidget->findChild<QVBoxLayout*>("verticalLayout_6");
 
@@ -21,14 +19,47 @@ ListSet::ListSet(QWidget *parent) :
     buttonClicked = -1;
     connect(ui->submit, &QPushButton::clicked, this, &ListSet::onSubmitClicked);
 
-    const std::string XMLFilePath ="../XJCO2811_UserInterface/videolist_data.xml";
+    const std::string XMLFilePath = "../XJCO2811_UserInterface/videolist_data.xml";
     fileUtil = new FileUtil(XMLFilePath);
     fileUtil->PrintAll();
     listsInfo = fileUtil->GetAllListsInfo();
+
+    // Set the input form to invisible at first time
+    ui->groupBox_right->setVisible(false);
+    ui->midline->setVisible(false);
+
+    for (size_t i = 0; i < listsInfo.size(); i++) {
+        // initialize video list ui
+        QPushButton* newButton = new QPushButton();
+        newButton->setText(listsInfo[i].name.c_str());
+        newButton->setCheckable(true);
+        newButton->setAutoExclusive(true);
+
+        listLayout->addWidget(newButton);
+
+        connect(newButton, &QPushButton::clicked, [this, newButton] {
+            ui->groupBox_right->setVisible(true);
+            ui->midline->setVisible(true);
+            ui->submit->setText(QString("Edit"));
+
+            int index = listLayout->indexOf(newButton) - 1;
+            buttonClicked = index;
+            qDebug() << "Button clicked. Index: " << index;
+            // Check if the index is valid
+            if (index != -1 && index < (int)this->listsInfo.size()) {
+                // Retrieve the corresponding ListInfo
+                ListInfo info = this->listsInfo[index];
+                // Set the text of editName and editPath
+                ui->editName->setText(QString::fromStdString(info.name));
+                ui->editPath->setText(QString::fromStdString(info.videoDirPath));
+            }
+        });
+
+        std::cout << listsInfo[i].name << std::endl;
+    }
 }
 
-ListSet::~ListSet()
-{
+ListSet::~ListSet() {
     delete ui;
 }
 
@@ -39,32 +70,41 @@ ListSet::~ListSet()
 int ListSet::on_addList_clicked() {
     clickCount++;
 
-    QPushButton *newButton = new QPushButton;
+    QPushButton* newButton = new QPushButton;
     newButton->setText("New List " + QString::number(clickCount));
+    newButton->setCheckable(true);
+    newButton->setAutoExclusive(true);
+
     listLayout->addWidget(newButton);
 
-    listsInfo = fileUtil->GetAllListsInfo();
-    fileUtil->PrintAll();
-    std::string error;
-    fileUtil->AddNewList(("New List " + QString::number(clickCount)).toStdString(), "../..", &error);
-    qDebug() << listsInfo.size();
+    // listsInfo = fileUtil->GetAllListsInfo();
+    // fileUtil->PrintAll();
+    // std::string error;
+    // fileUtil->AddNewList(("New List " + QString::number(clickCount)).toStdString(), "../..", &error);
+    // qDebug() << listsInfo.size();
 
     connect(newButton, &QPushButton::clicked, [this, newButton] {
-        int index = listLayout->indexOf(newButton);
+        ui->groupBox_right->setVisible(true);
+        ui->midline->setVisible(true);
+
+        ui->editName->setText(QString(""));
+        ui->editPath->setText(QString(""));
+
+        int index = listLayout->indexOf(newButton) - 1;
         buttonClicked = index;
         qDebug() << "Button clicked. Index: " << index;
         // Check if the index is valid
-        if (index != -1 && index < this->listsInfo.size()) {
+        if (index != -1 && index < (int)this->listsInfo.size()) {
             // Retrieve the corresponding ListInfo
-            ListInfo info = this->listsInfo[index];
+            // ListInfo info = this->listsInfo[index];
+
             // Set the text of editName and editPath
-            ui->editName->setText(QString::fromStdString(info.name));
-            ui->editPath->setText(QString::fromStdString(info.videoDirPath));
+            ui->editName->setText(QString(""));
+            ui->editPath->setText(QString(""));
         }
     });
     return clickCount;
 }
-
 
 // onSubmitClicked() handles the click event of the submit button.
 // It retrieves the list name and video directory path from the user interface,
@@ -76,11 +116,11 @@ void ListSet::onSubmitClicked() {
     std::string videoDirPath = ui->editPath->text().toStdString();
     if (buttonClicked != -1) {
         // Update the existing list in listsInfo
-        qDebug() << "enter!" << buttonClicked <<  listsInfo.size();
+        qDebug() << "enter!" << buttonClicked << listsInfo.size();
         if (buttonClicked != -1 && buttonClicked <= listsInfo.size()) {
             listsInfo[buttonClicked].name = listName;
             listsInfo[buttonClicked].videoDirPath = videoDirPath;
-            QPushButton *buttonIndex = qobject_cast<QPushButton*>(listLayout->itemAt(buttonClicked)->widget());
+            QPushButton* buttonIndex = qobject_cast<QPushButton*>(listLayout->itemAt(buttonClicked)->widget());
             buttonIndex->setText(QString::fromStdString(listName));
             QMessageBox::information(this, "Success", "List added successfully!\n");
         }
@@ -94,9 +134,8 @@ void ListSet::onSubmitClicked() {
             QMessageBox::warning(this, "Error", "Failed to add list!\n");
         }
         clickCount++;
-        QPushButton *newButton = new QPushButton;
+        QPushButton* newButton = new QPushButton;
         newButton->setText(QString::fromStdString(listName));
         listLayout->addWidget(newButton);
     }
 }
-
--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
-#include "listset.h"
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <QApplication>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "listset.h"
 
 int main(int argc, char** argv) {
-    const std::string XMLFilePath = "../XJCO2811_UserInterface/videolist_data.xml";
-    std::ofstream fileStream(XMLFilePath, std::ios::trunc);
-    if (fileStream.is_open()) {
-        fileStream << "<list>\n</list>" << std::endl;
-        fileStream.close();
-    }
+    //    const std::string XMLFilePath = "../XJCO2811_UserInterface/videolist_data.xml";
+    //    std::ofstream fileStream(XMLFilePath, std::ios::trunc);
+    //    if (fileStream.is_open()) {
+    //        fileStream << "<lists>\n</lists>" << std::endl;
+    //        fileStream.close();
+    //    }
     QApplication app(argc, argv);
 
     //MainWindow w;
     ListSet w;
     w.show();
-    int i;
-    int j;
 
     app.exec();
 }
