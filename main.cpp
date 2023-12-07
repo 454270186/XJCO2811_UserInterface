3dd68db8a117ff46931d49950ae97dfdc053bf6a
Erfei Yu <105307186+454270186@users.noreply.github.com>
2023-12-07 21:57:17 +0800

    fix: 还原main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <QApplication>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include "listset.h"
+
+#include "mainwindow.h"
 
 int main(int argc, char** argv) {
-    //    const std::string XMLFilePath = "../XJCO2811_UserInterface/videolist_data.xml";
-    //    std::ofstream fileStream(XMLFilePath, std::ios::trunc);
-    //    if (fileStream.is_open()) {
-    //        fileStream << "<lists>\n</lists>" << std::endl;
-    //        fileStream.close();
-    //    }
     QApplication app(argc, argv);
 
-    //MainWindow w;
-    ListSet w;
+    MainWindow w;
     w.show();
 
     app.exec();
