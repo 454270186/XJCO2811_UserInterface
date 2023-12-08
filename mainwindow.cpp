4d9f5dad54cdc789c608e1362536d337406c1ff3
Erfei Yu <105307186+454270186@users.noreply.github.com>
2023-12-08 17:46:56 +0800

    fix: fix some syntax error

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -228,16 +228,17 @@ void MainWindow::setMediaAndPlay() {
         // Increment the video index for the next iteration
         currentVideoIndex = (currentVideoIndex + 1) % videoPaths.size();
     }
-    void MainWindow::handleVideoSelection(const QStringList& videoPaths, int currentIndex) {
-        // 将视频路径数组和索引传递给接口
-        // 这里实现接口调用的逻辑
-        qDebug() << "Button clicked. Video paths:"
-                 << videoPaths;  // VideoPaths是一个字符串数组，存储文件夹里所有的视频路径
-        qDebug() << "Button clicked. currentIndex:" << currentIndex;  // currentIndex是一个整型，显示当前按钮对应的索引
-    }
+}
 
-    void MainWindow::switchToListset() {
-        hide();
-        ListSet* listsetWindow = new ListSet();
-        listsetWindow->show();
-    }
+void MainWindow::handleVideoSelection(const QStringList& videoPaths, int currentIndex) {
+    // 将视频路径数组和索引传递给接口
+    // 这里实现接口调用的逻辑
+    qDebug() << "Button clicked. Video paths:" << videoPaths;  // VideoPaths是一个字符串数组，存储文件夹里所有的视频路径
+    qDebug() << "Button clicked. currentIndex:" << currentIndex;  // currentIndex是一个整型，显示当前按钮对应的索引
+}
+
+void MainWindow::switchToListset() {
+    hide();
+    ListSet* listsetWindow = new ListSet();
+    listsetWindow->show();
+}
