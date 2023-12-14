afa302feb7dd114171d5d03c9d4d5408b1a154aa
Deck-Wang416 <sc212yw@leeds.ac.uk>
2023-12-14 18:31:23 +0800

    style: correct some mistakes.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <QApplication>
 
 #include "pagemanager.h"
-// #include "listset.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
     PageManager pagemanager;
     pagemanager.show();
-
-    // ListSet* listSet = new ListSet();
-    // listSet->show();
 
     app.exec();
 }
