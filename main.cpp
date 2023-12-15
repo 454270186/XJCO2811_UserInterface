9078476a15d3376abc5127170c70fd4653befef2
Yue <sc21y2h@leeds.ac.uk>
2023-12-15 22:14:38 +0800

    style: format main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,8 @@
 #include <QApplication>
-#include <QFile>
 #include "pagemanager.h"
-#include <QString>
-#include <QDir>
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
-    QFile file("../XJCO2811_UserInterface/mainwindow.qss");
-    if (file.open(QFile::ReadOnly)) {
-        QString StyleSheet = QLatin1String(file.readAll());
-        app.setStyleSheet(StyleSheet);
-        file.close();
-    } else {
-    qDebug() << "File does not exist: " << file.fileName();
-    qDebug() << "Current directory:" << QDir::currentPath();
-
-    }
 
     PageManager pagemanager;
     pagemanager.show();
