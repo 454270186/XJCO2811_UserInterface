a483acb835c3822288b5003411c75628b3997873
Erfei Yu <105307186+454270186@users.noreply.github.com>
2023-12-20 14:19:36 +0800

    test: test ffmpeg

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <QApplication>
 #include "pagemanager.h"
 
+#include <QProcess>
+
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    PageManager pagemanager;
-    pagemanager.show();
+    //    PageManager pagemanager;
+    //    pagemanager.show();
+
+    QString ffmpegCommand =
+        "../XJCO2811_UserInterface/bin/ffmpeg -i ../XJCO2811_UserInterface/videos/demo/wang.mp4 -vframes 1 "
+        "../XJCO2811_UserInterface/output.jpg";
+    QProcess ffmpegProcess;
+    ffmpegProcess.start(ffmpegCommand);
+    ffmpegProcess.waitForFinished();
 
     app.exec();
 }
