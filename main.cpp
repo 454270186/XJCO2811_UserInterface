b96cb62915cdc31ea6c50bb94af8040669e2d1ac
Erfei Yu <105307186+454270186@users.noreply.github.com>
2023-12-20 15:01:56 +0800

    fix: reset main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <QApplication>
 #include "pagemanager.h"
 
-#include <QProcess>
-
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    //    PageManager pagemanager;
-    //    pagemanager.show();
-    QProcess ffmpegProcess;
-    ffmpegProcess.setProgram("../XJCO2811_UserInterface/bin/ffmpeg");
-    ffmpegProcess.setArguments({"-i", "../XJCO2811_UserInterface/videos/demo/wang.mp4", "-vframes", "1",
-                                "../XJCO2811_UserInterface/output.jpg"});
-
-    ffmpegProcess.start();
-    ffmpegProcess.waitForFinished();
+    PageManager pagemanager;
+    pagemanager.show();
 
     app.exec();
 }
